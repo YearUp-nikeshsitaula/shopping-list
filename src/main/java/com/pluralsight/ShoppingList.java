fd0aea645783b36aa852837a933c8144487c5114
Nikesh Sitaula <sitaulanike@gmail.com>
2024-09-25 15:15:15 -0400

    completed ShoppingList app

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Shopping list:");
+        System.out.println("-------------------");
+        System.out.println("Apples");
+        System.out.println("Olives");
+        System.out.println("Mangoes");
+        System.out.println("Bananas");
+        System.out.println("Oranges");
+    }
+}
